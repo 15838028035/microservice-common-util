5903fbafa58976aa50d442b4a447d3396f47e840
15838028035@163.com <you@example.com>
2018-08-09 19:11:22 +0800

    sonar bug 修改

--- src/test/java/com/zhongkexinli/micro/serv/common/constant/CommonConstantsTest.java
+++ src/test/java/com/zhongkexinli/micro/serv/common/constant/CommonConstantsTest.java
@@ -9,25 +9,25 @@ public class CommonConstantsTest {
 
 	@Test
 	public void test() {
-		assertEquals(CommonConstants.RESOURCE_TYPE_MENU,"menu","excepted");
-		assertEquals(CommonConstants.RESOURCE_TYPE_BTN,"button","excepted");
+		assertEquals("menu",CommonConstants.RESOURCE_TYPE_MENU,"excepted menu");
+		assertEquals("button",CommonConstants.RESOURCE_TYPE_BTN,"excepted button");
 		
 		assertTrue(CommonConstants.EX_USER_INVALID_CODE>0);
 		assertTrue(CommonConstants.EX_CLIENT_FORBIDDEN_CODE>0);
 		assertTrue(CommonConstants.EX_OTHER_CODE>0);
 		
-		assertEquals(CommonConstants.CONTEXT_KEY_USER_ID,"currentUserId","excepted");
-		assertEquals(CommonConstants.CONTEXT_KEY_USERNAME,"currentUserName","excepted");
-		assertEquals(CommonConstants.CONTEXT_KEY_USER_NAME,"currentUser","excepted");
+		assertEquals("currentUserId",CommonConstants.CONTEXT_KEY_USER_ID,"excepted currentUserId");
+		assertEquals("currentUserName",CommonConstants.CONTEXT_KEY_USERNAME,"excepted currentUserName");
+		assertEquals("currentUser",CommonConstants.CONTEXT_KEY_USER_NAME,"excepted currentUser");
 		
-		assertEquals(CommonConstants.CONTEXT_KEY_USER_TOKEN,"currentUserToken","excepted");
-		assertEquals(CommonConstants.JWT_KEY_USER_ID,"userId","excepted");
-		assertEquals(CommonConstants.JWT_KEY_NAME,"name","excepted");
-		assertEquals(CommonConstants.ENCODING_UTF_8,"UTF-8","excepted");
+		assertEquals("currentUserToken",CommonConstants.CONTEXT_KEY_USER_TOKEN,"excepted currentUserToken");
+		assertEquals("userId",CommonConstants.JWT_KEY_USER_ID,"excepted userId");
+		assertEquals("name",CommonConstants.JWT_KEY_NAME,"excepted name");
+		assertEquals("UTF-8",CommonConstants.ENCODING_UTF_8,"excepted UTF-8");
 		
 		
-		assertEquals(CommonConstants.FAIL,"0","excepted");
-		assertEquals(CommonConstants.SUCCESS_MSG,"成功","excepted");
+		assertEquals("0",CommonConstants.FAIL,"excepted 0");
+		assertEquals("成功",CommonConstants.SUCCESS_MSG,"excepted 成功");
 		
 		assertTrue(CommonConstants.ERROR==0);
 		assertTrue(CommonConstants.SUCCESS==1);
