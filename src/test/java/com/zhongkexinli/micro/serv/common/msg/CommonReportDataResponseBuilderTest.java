4de38f0cdbcd129374aa84e8266bbfe0c9ce5da1
liujie <liujie@163.com>
2021-02-05 09:15:22 +0800

    添加单元测试

---
+++ src/test/java/com/zhongkexinli/micro/serv/common/msg/CommonReportDataResponseBuilderTest.java
@@ -0,0 +1,19 @@
+package com.zhongkexinli.micro.serv.common.msg;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CommonReportDataResponseBuilderTest {
+
+    @Test
+    public void builder1Test() {
+        CommonReportDataResponse commonReportDataResponse =  new CommonReportDataResponseBuilder().code("1").msg("查询成功").count(100L).headers(Arrays.asList("header1","header2")).builder();
+        Assert.assertEquals("1",commonReportDataResponse.getCode());
+        Assert.assertEquals("查询成功",commonReportDataResponse.getMsg());
+        Assert.assertTrue(commonReportDataResponse.getHeaders().size()==2);
+    }
+
+}
