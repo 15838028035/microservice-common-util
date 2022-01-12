087186e03ec345243d3cd7357b511e3b2caa8847
15838028035 <15838028035@163.com>
2022-01-12 15:35:20 +0800

    优化工具类

--- src/main/java/com/zhongkexinli/micro/serv/common/util/FileUtil.java
+++ src/main/java/com/zhongkexinli/micro/serv/common/util/FileUtil.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -272,4 +273,77 @@ public class FileUtil {
         }
         return total;
     }
+    
+    public static void deleteDirectory(File directory) throws IOException {
+        if (!directory.exists()) {
+          return;
+        }
+
+        cleanDirectory(directory);
+        if (!directory.delete()) {
+          String message = "Unable to delete directory " + directory + ".";
+          throw new IOException(message);
+        }
+      }
+
+      public static boolean deleteQuietly(File file) {
+        if (file == null)
+          return false;
+        try {
+          if (file.isDirectory())
+            cleanDirectory(file);
+        } catch (Exception e) {
+        }
+        try {
+          return file.delete();
+        } catch (Exception e) {
+        }
+        return false;
+      }
+
+      public static void cleanDirectory(File directory) throws IOException {
+        if (!directory.exists()) {
+          String message = directory + " does not exist";
+          throw new IllegalArgumentException(message);
+        }
+
+        if (!directory.isDirectory()) {
+          String message = directory + " is not a directory";
+          throw new IllegalArgumentException(message);
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) {
+          throw new IOException("Failed to list contents of " + directory);
+        }
+
+        IOException exception = null;
+        for (int i = 0; i < files.length; i++) {
+          File file = files[i];
+          try {
+            forceDelete(file);
+          } catch (IOException ioe) {
+            exception = ioe;
+          }
+        }
+
+        if (null != exception)
+          throw exception;
+      }
+
+      public static void forceDelete(File file) throws IOException {
+        if (file.isDirectory()) {
+          deleteDirectory(file);
+        } else {
+          boolean filePresent = file.exists();
+          if (!file.delete()) {
+            if (!filePresent) {
+              throw new FileNotFoundException("File does not exist: " + file);
+            }
+            String message = "Unable to delete file: " + file;
+
+            throw new IOException(message);
+          }
+        }
+      }
 }
